2aea21d4b54dc656e0cbfd654ca445b112e8f4ce
Shigemi ISHIDA <ishida+devel@f.ait.kyushu-u.ac.jp>
2014-07-13 13:36:27 +0900

    Modify version comparison: * version comparison should use Chef::VersionConstraint.

--- recipes/repo.rb
+++ recipes/repo.rb
@@ -3,7 +3,7 @@ case node['platform']
 when 'ubuntu'
   include_recipe 'apt::default'
   # Ubuntu 14.04+ appears to have the right packages
-  if node['platform_version'].to_f < 12.04
+  if Chef::VersionConstraint.new("< 12.04").include?(node['platform_version'])
     # May not be needed in modern ubuntu, debian.
     apt_repository 'mosquitto' do
       uri          'http://repo.mosquitto.org/debian'
@@ -17,7 +17,7 @@ when 'debian'
   include_recipe 'apt::default'
 
   # Debian 7.0 or lower need the repo (wheezy)
-  if node['platform_version'].to_i <= 7
+  if Chef::VersionConstraint.new("<= 7.0").include?(node['platform_version'])
     # May not be needed in modern ubuntu, debian.
     apt_repository 'mosquitto' do
       uri          'http://repo.mosquitto.org/debian'
@@ -31,14 +31,15 @@ when "mac_os_x"
 when "redhat", "centos", "scientific", "fedora", "arch", "amazon"
   include_recipe 'yum::default'
   
-  if node['platform_version'].to_i == 6
+  if Chef::VersionConstraint.new(">= 6.0").include?(node['platform_version']) &&
+      Chef::VersionConstraint.new("< 7.0").include?(node['platform_version'])
     yum_repository 'mosquitto' do
       description 'Mosquitto Repository'
       baseurl     'http://download.opensuse.org/repositories/home:/oojah:/mqtt/CentOS_CentOS-6/'
       gpgkey      'http://download.opensuse.org/repositories/home:/oojah:/mqtt/CentOS_CentOS-6/repodata/repomd.xml.key'
       action :create
     end
-  elsif node['platform_version'].to_i == 5
+  elsif Chef::VersionConstraint.new(">= 5.0").include?(node['platform_version'])
     yum_repository 'mosquitto' do
       description 'Mosquitto Repository'
       baseurl     'http://download.opensuse.org/repositories/home:/oojah:/mqtt/CentOS_CentOS-5/'
